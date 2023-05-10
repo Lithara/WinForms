f61a57649334a66501d2818ede43916e58443c7e
Lithara Perera <litharaperera2002@gmail.com>
2023-05-10 10:04:40 +0530

    04

--- Krypton_Toolkit_Demo/Krypton_Toolkit_Demo/Form1.cs
+++ Krypton_Toolkit_Demo/Krypton_Toolkit_Demo/Form1.cs
@@ -22,26 +22,5 @@ namespace Krypton_Toolkit_Demo
         {
 
         }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string Name = textBox1.Text;
-            label2.Text = "Hello " + Name;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            label2.Text = "";
-        }
     }
 }
